d248ffe7d886cf81ed4971499674e6833313fc5d
Paul D'Amico <pauldamico88@gmail.com>
2022-09-05 15:24:47 -0400

    updated html file locations for github pages

---
+++ business-time-project/pages.js
@@ -0,0 +1,5 @@
+document.body.style.backgroundColor="rgb(213 213 213)"
+document.getElementById("services").addEventListener("click", ()=>{location.href ="./services.html"})
+document.getElementById("gallery").addEventListener("click", ()=>{location.href ="./gallery.html"})
+document.getElementById("contact").addEventListener("click", ()=>{location.href ="./contact.html"})
+document.getElementById("home").addEventListener("click", ()=>{location.href ="../index.html"})
